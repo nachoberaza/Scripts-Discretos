ac657e94e48867b4418266b6be523a8049b120fc
nachoberaza@gmail <nachoberaza@gmail.com>
2021-04-27 13:03:42 -0300

    Corrigiendo unos errores.

--- Funciones logicas/Comparacion equivalencias proposiciones.py
+++ Funciones logicas/Comparacion equivalencias proposiciones.py
@@ -1,92 +1,64 @@
-# Script, en paradigma orientado a objetos pensado para comparar 
+# Script, en paradigma estructurado con elementos de POO, pensado para comparar 
 # y verificar los ejercicios de simplificacion
 # de proposiciones logicas de la unidad 1
 # mediante el uso de la tabla de verdad.
 
 #Limpia la consola al iniciar
 import os
-os.system('cls')
+os.system('clear')
 
-class Funcion_booleana:
+class Operation:
 
-    def __init__(self, quantity_of_variables):
-        self.quantity_of_variables=quantity_of_variables
-    
-
-
-# # ------ Proposiciones de 2 variables ------ #
-
-# #La proposicion original presteada 
-# def calculate(p,q):
-#     result=bool(False)
-    
-#     #Proposiciones original
-#     #result=
+    #La funcion tiene que estar preseteada 
+    def calculate(self,p,q,r):
+        result=bool(False)
         
-#     return result
+        #Proposicion original
+        #result=    
+        return result
 
-# #La proposicion a la que se quiere comparar preseteada
-# def calculateToComparate(p,q):
-#     result=bool(False)
-    
-#     #Proposicion original
-#     #result = 
+    def calculateToComparate(self,p,q,r):
+        result=bool(False)
         
-#     return result
+        #Proposicion simplificada propuesta
+        #result =         
 
-# # ------ ************************ ------#
+        return result
 
-# # ------ Proposiciones de 3 variables ------ #
+def print_table(option) :
+    operation=Operation()
 
-# #La funcion tiene que estar preseteada 
-# def calculate(p,q,r):
-#     result=bool(False)
-    
-#     #Proposicion original
-#     #result=    
-#     return result
+    if ( option == 2 ):
+        print( "  P || Q || f(p,q) || Funcion propuesta " )
+        print(" ======================================== ")
+        print ("  F |  F  |   " +"     "+ str( operation.calculate(False,False,None) ) +"     "+ str( operation.calculateToComparate(False,False,None) ) )
+        print ("  F |  V  |   " +"     "+ str( operation.calculate(False,True,None) )  +"     "+ str( operation.calculateToComparate(False,True,None) ) )
+        print ("  V |  F  |   " +"     "+ str( operation.calculate(True,False,None) )  +"     "+ str( operation.calculateToComparate(True,False,None) ) )
+        print ("  V |  F  |   " +"     "+ str( operation.calculate(True,False,None) )  +"     "+ str( operation.calculateToComparate(True,False,None) ) )
+        print ("  V |  V  |   " +"     "+ str( operation.calculate(True,True,None) )   +"     "+ str( operation.calculateToComparate(True,True,None) ) )
 
-# def calculateToComparate(p,q,r):
-#     result=bool(False)
-    
-#     #Proposicion simplificada propuesta
-#     #result =         
-#     return result
+    elif( option == 3 ):
 
-# # ------ ************************ ------#
-
-# def print_table(option) :
-
-#     if ( option == 2 ):
-#         print( "  P || Q || f(p,q) || Funcion propuesta " )
-#         print(" ======================================== ")
-#         print ("  F |  F  |   " +"     "+ str( calculate(False,False) ) +"     "+ str( calculateToComparate(False,False) ) )
-#         print ("  F |  V  |   " +"     "+ str( calculate(False,True) )  +"     "+ str( calculateToComparate(False,True) ) )
-#         print ("  V |  F  |   " +"     "+ str( calculate(True,False) )  +"     "+ str( calculateToComparate(True,False) ) )
-#         print ("  V |  V  |   " +"     "+ str( calculate(True,True) )   +"     "+ str( calculateToComparate(True,True) ) )
-
-#     elif( option == 3 ):
-
-#         print( "  P || Q || R || f(p,q) || Funcion propuesta " )
-#         print(" ============================================= ")
-#         print ("  F |  F  | F " +"     "+ str( calculate(False,False,False) )+"     "+ str( calculateToComparate(False,False,False) ) )
-#         print ("  F |  F  | V " +"     "+ str( calculate(False,False,True) ) +"     "+ str( calculateToComparate(False,False,True) ) )
-#         print ("  F |  V  | F " +"     "+ str( calculate(False,True,False) ) +"     "+ str( calculateToComparate(False,True,False) ) )
-#         print ("  F |  V  | V " +"     "+ str( calculate(False,True,True) )  +"     "+ str( calculateToComparate(False,True,True) ) )
-#         print ("  V |  F  | F " +"     "+ str( calculate(True,False,False) ) +"     "+ str( calculateToComparate(True,False,False) ) )
-#         print ("  V |  F  | V " +"     "+ str( calculate(True,False,True) )  +"     "+ str( calculateToComparate(True,False,True) ) )
-#         print ("  V |  V  | F " +"     "+ str( calculate(True,True,False) )  +"     "+ str( calculateToComparate(True,True,False) ) )
-#         print ("  V |  V  | V " +"     "+ str( calculate(True,True,True) )   +"     "+ str( calculateToComparate(True,True,True) ) )
+        print( "  P || Q || R || f(p,q) || Funcion propuesta " )
+        print(" ============================================= ")
+        print ("  F |  F  | F " +"     "+ str( operation.calculate(False,False,False) )+"     "+ str( operation.calculateToComparate(False,False,False) ) )
+        print ("  F |  F  | V " +"     "+ str( operation.calculate(False,False,True) ) +"     "+ str( operation.calculateToComparate(False,False,True) ) )
+        print ("  F |  V  | F " +"     "+ str( operation.calculate(False,True,False) ) +"     "+ str( operation.calculateToComparate(False,True,False) ) )
+        print ("  F |  V  | V " +"     "+ str( operation.calculate(False,True,True) )  +"     "+ str( operation.calculateToComparate(False,True,True) ) )
+        print ("  V |  F  | F " +"     "+ str( operation.calculate(True,False,False) ) +"     "+ str( operation.calculateToComparate(True,False,False) ) )
+        print ("  V |  F  | V " +"     "+ str( operation.calculate(True,False,True) )  +"     "+ str( operation.calculateToComparate(True,False,True) ) )
+        print ("  V |  V  | F " +"     "+ str( operation.calculate(True,True,False) )  +"     "+ str( operation.calculateToComparate(True,True,False) ) )
+        print ("  V |  V  | V " +"     "+ str( operation.calculate(True,True,True) )   +"     "+ str( operation.calculateToComparate(True,True,True) ) )
         
-#     else:
-#         print("Aun no se programaron proposiciones con esa cantidad de variables")
+    else:
+        print("Aun no se programaron proposiciones con esa cantidad de variables")
 
-#     print ("\n")
+    print ("\n")
 
-# #--------------------------------------------------------------------------------------------------------#
+#--------------------------------------------------------------------------------------------------------#
 
-# #Comienzo de la ejecucion
-# option=int(input("Ingrese la cantidad de variables que tiene la proposicion a evaluar: "))
-# print_table(option)
+#Comienzo de la ejecucion
+option=int(input("Ingrese la cantidad de variables que tiene la proposicion a evaluar: "))
+print_table(option)
 
-# print("Finalizando script...")
+print("Finalizando script...")
